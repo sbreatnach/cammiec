d4d44c149a2b17c2062450e836e77ecd2accb34e
Shane Breatnach <shane.breatnach@gmail.com>
2016-06-12 13:59:35 +0100

    Latest changes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <string>
 #include <boost/lexical_cast.hpp>
+#include <opencv2/opencv.hpp>
 #include "mjpeg_server.hpp"
 
-cv::Mat
-random_image()
-{
-  return cv::Mat(cv::Size(640, 480), CV_8UC3, cv::Scalar(std::rand() % 255, std::rand() % 255, std::rand() % 255));
-}
 int
 main(int argc, char* argv[])
 {
@@ -27,21 +23,23 @@ main(int argc, char* argv[])
     server_ptr s(init_streaming_server(argv[1], argv[2], argv[4], num_threads));
     streamer_ptr stmr(new streamer);
     register_streamer(s, stmr, "/stream_0");
-    streamer_ptr stmr2(new streamer);
-    register_streamer(s, stmr2, "/stream_1");
-    streamer_ptr stmr3(new streamer);
-    register_streamer(s, stmr3, "/foobar");
     s->start();
     std::cout << "Visit:\n" << argv[1] << ":" << argv[2] << "/stream_0" << std::endl;
-    while (true)
-    {
-      cv::Mat image1 = random_image(), image2 = random_image(), image3 = random_image();
+
+    cv::VideoCapture stream1(0);
+    if (!stream1.isOpened()) {
+      std::cout << "cannot open camera";
+      return -2;
+    }
+
+    while (true) {
+      cv::Mat cameraFrame;
+      stream1.read(cameraFrame);
+
       bool wait = false; //don't wait for there to be more than one webpage looking at us.
       int quality = 80;
-      stmr->post_image(image1,quality, wait);
-      stmr2->post_image(image2,quality, wait);
-      stmr3->post_image(image3,quality, wait);
-      boost::this_thread::sleep(boost::posix_time::milliseconds(100)); //10 fps for ease on my eyes
+      stmr->post_image(cameraFrame,quality, wait);
+      boost::this_thread::sleep(boost::posix_time::milliseconds(5000)); //10 fps for ease on my eyes
     }
   } catch (std::exception& e)
   {
